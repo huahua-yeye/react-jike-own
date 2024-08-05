a8203ff6e6c99417bbae09d85dd571b8d0d61461
huahua-yeye <yucenhua@bupt.edu.cn>
2024-08-05 13:57:59 +0800

    new features

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <div>changes for new-features branch</div>
   </React.StrictMode>
 );
 
