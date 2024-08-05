2edd02c947af7644f1baa70ffd2a843a2896d60a
huahua-yeye <yucenhua@bupt.edu.cn>
2024-08-05 14:21:27 +0800

    submit test

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <div>changes for new-features branch</div>
+    <div>changes for new-features branch2</div>
   </React.StrictMode>
 );
 
